608c7dbb60f17fb1e2f16ad48df643b06ee7ee33
SoraOoshiro <e195740@ie.u-ryukyu.ac.jp>
2021-07-27 13:16:52 +0900

    parallel

--- julia_parallel1.py
+++ julia_parallel1.py
@@ -1,21 +1,18 @@
+from multiprocessing import Pool
 import numpy as np
 import matplotlib.pyplot as plt
 
 def julia(max, comp):
     re, im = comp[0], comp[1]
-    #実部が-0.7、虚部が-0.3の複素数cを作成(ここの数値を変えればさまざまなジュリア集合を作れる)
     c = complex(-0.7, -0.3)
-
-    #実部がre、虚部がimの複素数zを作成
     z = complex(re, im)
 
     for i in range(max):
         z = z*z + c
-        #zの絶対値が一度でも2を超えればzが発散することを利用
         if abs(z) >= 2:
-            return i        #発散する場合には何回目のループで終わったかを返す
+            return i
     
-    return max     #無限大に発散しない場合にはmaxを返す
+    return max
 
 def bet_julia(list):
     julia_list = []
@@ -24,11 +21,11 @@ def bet_julia(list):
     
     return julia_list
 
+
 def main():
     re = np.linspace(-2, 2, 2000)
     im = np.linspace(2, -2, 2000)
 
-    #実部と虚部の組み合わせを作成
     Re, Im = np.meshgrid(re, im)
     comp = np.c_[Re.ravel(), Im.ravel()]
 
@@ -42,29 +39,24 @@ def main():
         # print(slice_list[y][:5])
 
     J = []
-    for i in slice_list:
-        J.extend(bet_julia(i))
-
-    '''
-    #計算結果を格納するための零行列を作成
-    Julia = np.zeros(len(comp))
-
-    #マンデルブロ集合に属するかの計算
-    for i, c_point in enumerate(comp):
-        Julia[i] = julia(200, c_point)
-    '''
+    
+    with Pool(processes=4) as p:
+        result_list = p.map(func=bet_julia, iterable=slice_list)
+        # print(result_list)
+    
+    for i in result_list:
+        J.extend(i)
 
     Julia = np.array(J)
     Julia = Julia.reshape((2000, 2000))
-
     fig = plt.figure(dpi=600)
     plt.axis("off")
     plt.imshow(Julia, cmap="gray", extent=[-2, 2, -2, 2])
-    fig.set_size_inches(3, 3)        #出力される画像の大きさを調整
+    fig.set_size_inches(3, 3)
     # plt.show()
 
-    #画像を保存
     fig.savefig("julia_2.png")
+    
 
 if __name__ == "__main__":
     main()
